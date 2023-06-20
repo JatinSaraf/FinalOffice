7a5fe5c9ecc14535eeded85b9fcd0b4d46261e30
Jatin Saraf <jats1118@gmail.com>
2023-06-20 13:03:52 +0530

    Signup Done frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,23 +1,23 @@
-// import React from 'react'
-// import { Login } from "../components/Login/Login.jsx";
-
-// const LoginPage = () => {
-//   return (
-//     <div>
-//         <Login />
-//     </div>
-//   )
-// }
-
-// export default LoginPage;
-
 import React from 'react'
 import Login from "../components/Login/Login.jsx";
 
-export const LoginPage = () => {
+const LoginPage = () => {
   return (
     <div>
         <Login />
-        </div>
+    </div>
   )
 }
+
+export default LoginPage;
+
+// import React from 'react'
+// import Login from "../components/Login/Login.jsx";
+
+// export const LoginPage = () => {
+//   return (
+//     <div>
+//         <Login />
+//         </div>
+//   )
+// }
---
+++ frontend/src/pages/SignupPage.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Signup from '../components/Signup/Signup.jsx';
+
+const SignupPage = () => {
+  return (
+    <div>
+        <Signup/>
+    </div>
+  )
+}
+
+export default SignupPage
+
+// import React from 'react'
+// import Signup from '../components/Signup/Signup';
+
+// export const SignupPage = () => {
+//   return (
+//     <div>
+//         <Signup/>
+
+//     </div>
+//   )
+// }
