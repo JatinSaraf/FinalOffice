67424e2e414799359761d3be55cbe26d817d25c4
Jatin Saraf <jats1118@gmail.com>
2023-06-20 12:07:10 +0530

    Login Done frontend

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,23 @@
+// import React from 'react'
+// import { Login } from "../components/Login/Login.jsx";
+
+// const LoginPage = () => {
+//   return (
+//     <div>
+//         <Login />
+//     </div>
+//   )
+// }
+
+// export default LoginPage;
+
+import React from 'react'
+import Login from "../components/Login/Login.jsx";
+
+export const LoginPage = () => {
+  return (
+    <div>
+        <Login />
+        </div>
+  )
+}
